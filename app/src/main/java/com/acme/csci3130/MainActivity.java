bacb80dec576e7480ef38ef0c790d6fb6484833c
Jesse Carter <m.c.jesse@live.com>
2017-05-09 10:54:57 -0300

    Assignment 1 Part 3 Changes Made :)

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,27 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView display = (TextView) findViewById(R.id.helloText);
+        final EditText enter = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(enter.getText().toString());
+            }
+        });
     }
+
 }
